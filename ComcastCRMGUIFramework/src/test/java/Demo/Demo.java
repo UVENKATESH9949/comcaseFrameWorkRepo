aa53e112bf37e6f5a3d46fe7e5bda8d6dcd71bb8
Undekode Venkatesh <venkatesh9949.u@gmail.com>
2025-12-24 15:58:22 +0530

    adding method twise

--- ComcastCRMGUIFramework/src/test/java/Demo/Demo.java
+++ ComcastCRMGUIFramework/src/test/java/Demo/Demo.java
@@ -2,6 +2,13 @@ package Demo;
 
 public class Demo {
 
+	static void add() {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		
+		System.out.println(c);
+	}
 	public static void main(String[] args) {
 		System.out.println("Demo commit ");
 		System.out.println("Demo commit 2 ");
